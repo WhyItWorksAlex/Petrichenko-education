e6bcd2105cea441740aec61050d707a9ea5d15b6
WhyItWorksAlex <fishman1q2w3e@gmail.com>
2024-08-05 00:48:03 +0300

    Изменения

--- js/script.js
+++ js/script.js
@@ -112,5 +112,27 @@ function transferWaitors(data) {
   return copy;
 }
 
-transferWaitors(restorantData);
-console.log(restorantData)
+// transferWaitors(restorantData);
+// console.log(restorantData)
+
+const inputRub = document.querySelector('#rub');
+const inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', 'js/current.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
+
+  request.addEventListener('readystatechange', () => {
+    if(request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+    } else {
+      inputUsd.value = 'Что то пошло не так';
+    }
+  })
+
+})
